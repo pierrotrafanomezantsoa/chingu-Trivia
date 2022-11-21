f87ee287231d83bada941c62512162d9cb335313
pierrotrafanomezantsoa <109613677+pierrotrafanomezantsoa@users.noreply.github.com>
2022-11-21 12:14:08 +0300

    Add files via upload

---
+++ assets/quizQuestions.js
@@ -0,0 +1,22 @@
+//======== Lists of Questions ==================================
+var questions = ["Which of these CSS properties allows you to add a shadow to letters?",
+                 "Which of these CSS rules allows you to change the appearance of a previously visited link?",
+                 "Which of these tags represents a row in a table?",
+                 "What is the most recommended form submission method that allows you to submit the most data?",
+                 "Quelle balise permet aussi bien d'insérer des cases à cocher, des champs de texte monolignes et des sélecteurs de date ?",
+                 "Which CSS directive allows you to create a responsive web design site?", 
+                 "Which of the following propositions is a valid variable declaration and initialization in Javascript?",
+                 "We declare an array in JS such as list of elements, separated by comma between ...",
+                 "Which HTML5 tag should be used for groups of navigational links?", 
+                 "Wich variable declaration is used for values likely to change"];
+//=========== Lists of answer ==================================
+var answers = ["Number1", "Number2", "Number1", "Number3", "Number1", "Number1", "Number2", "Number3", "Number4", "Number1"];
+//========== The true answer : =================================
+/*
+* Answers: box-shadow, a:visited, tr, post, input, @media, let myVariable = 12; [] square brackets, nav, var
+*/
+//============= Des choix possibles ============================
+var choiceButton1 = ["box-shadow", "a:consulted", "tr", "get", "input ","@media", "let my variable = 12; ", "{} braces","header", "var"];
+var choiceButton2 = ["text-shadow", "a:visited", "table", "action", "form","screensize", "let myVariable = 12;", "[] square brackets","navigation", "const"];
+var choiceButton3 = ["letter-shadow", "a:focus", "line", "post", "textarea","handheld", "variable myVariable = 12;", "() parenthesis","main", "variable"];
+var choiceButton4 = ["shadow","a:hover", "td", "submit","select","responsive", "let 12 = myVariable", "None of these lists","nav", "let"];
